4780cc1338f404a3824c10741818b22f89444be5
Lars-Ostervold <ostervold.berent@gmail.com>
2024-04-05 11:43:07 -0500

    remove emoji

--- 1_🧠_BibleBuddy.py
+++ 1_🧠_BibleBuddy.py
@@ -226,8 +226,8 @@ if st.session_state.show_wip:
 
 cols = st.columns([5,1])
 with cols[0]:
-    st.title("🧠BibleBuddy Chatbot")
-    st.subheader("Ask me a question about the Bible and I'll answer from the BibleProject archives! 📖🤖")
+    st.title("BibleBuddy Chatbot")
+    st.subheader("Ask me a question about the Bible and I'll answer from the BibleProject archives!")
 with cols[1]:
     st.image("resources/images/logo.png", width=100)
     hide_disclaimer = st.checkbox("Hide disclaimer", value=False)
